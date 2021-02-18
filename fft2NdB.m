2a39bac693192fd5224041b0e91f007c1321858c
TheHugo <the.hugo.lom@gmail.com>
2021-02-18 19:20:11 +0100

    upg3 testing material (missing window)

--- fft2NdB.m
+++ fft2NdB.m
@@ -1,5 +1,9 @@
-function [outputArg1,outputArg2] = fft2NdB(y,fs)
+function [Y,f] = fft2NdB(y,fs)
 %FFT2NDB transforms the signal to the frequency-domain, normalised 
-    
+    N = length(y); % number of points
+    Fy = fft(y,N)/N; % F{y}
+    P2 = abs(Fy);   % symmetric
+    Y = 20*log10(P2(1:N/2));
+    f = linspace(0,fs/2,N/2);
 end
 
--- wavesquare.m
+++ wavesquare.m
@@ -2,6 +2,6 @@ function a = wavesquare(f,t)
 %WAVESQUARE Summary of this function goes here
 %   Detailed explanation goes here
     p = 1./f;
-    a = round(mod(t,p)./p);
+    a = 2*round(mod(t,p)./p) - 1;
 end
 
